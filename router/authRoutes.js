1b2076ac66354ba5273df071eb9debc94497386b
Venktesh123 <8303531530venktesh@gmail.com>
2024-12-15 00:19:15 +0530

    jwt token added in every api

--- router/authRoutes.js
+++ router/authRoutes.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const { login, SignUp } = require("../controller/auhController");
-console.log("kl");
 router.post("/signup", SignUp);
 router.post("/login", login);
 module.exports = router;
